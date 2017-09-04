d3b3cd3888c2e142f66f5bcb28960a508584080e
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2017-09-05 02:20:22 +0530

    Update myna23.c

--- myna23.c
+++ myna23.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main() 
 {
-int a,b,i,hcf;
+int a1,b,i,hcf;
 printf("enter two values\t");
-scanf("%d%d",&a,&b);
-for(i=1;i<= a||i<=b;i++) 
+scanf("%d%d",&a1,&b);
+for(i=1;i<= a1||i<=b;i++) 
 {
-if(a%i==0 && b%i==0)
+if(a1%i==0 && b%i==0)
 hcf=i;
 }
 printf("HCF=%d",hcf);
