dc6ab5fde933fb63ad6728173d92b4f8f56b69e1
mynavathithavamaniramas <30829075+mynavathithavamaniramas@users.noreply.github.com>
2018-02-12 16:57:37 +0530

    Update myna23.c

--- myna23.c
+++ myna23.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 int main() 
 {
-int a1,b,i,hcf;
-printf("enter two values\t");
-scanf("%d%d",&a1,&b);
-for(i=1;i<= a1||i<=b;i++) 
-{
-if(a1%i==0 && b%i==0)
-hcf=i;
-}
-printf("HCF=%d",hcf);
-return 0;
+  int i,n;
+  int a[100];
+  printf("enter the 1 to 100");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+  {
+    printf("the number is:"i+1);
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<=n;i++)
+  {
+    if(a[0]>a[i])
+      a[0]=a[i];
+  }
+  printf("the smallest element is%d",a[0]);
 }
